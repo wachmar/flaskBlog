37ed92a1e0694ca258ea2888b89898aaf8d06fe7
Martin <martin.wachtl@grandit.cz>
2022-05-24 15:43:31 +0200

    initial commit

---
+++ puppyblog/__init__.py
@@ -0,0 +1,36 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+# FORMS >> TOENV
+app.config['SECRET_KEY'] = 'mysecret'
+
+## DB SETUP
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+## LOGIN CONF
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+from puppyblog.core.views import core
+from puppyblog.users.views import users
+from puppyblog.blogposts.views import blog_posts
+from puppyblog.error_pages.handlers import error_pages
+
+## BLUEPRINTS
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
+app.register_blueprint(users)
+app.register_blueprint(blog_posts)
